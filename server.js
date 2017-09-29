640c3923866fa1cde16ad2c0bcc4d83565d70972
Nenad Percic <n.percic@vega.local>
2017-09-29 15:26:45 +0200

    adding sessions get request support

--- server.js
+++ server.js
@@ -9,16 +9,32 @@ app.set('port', process.env.PORT || 5000)
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/speakers', function(request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT full_name__c, picture_path__c, session__c FROM salesforce.Speaker__c', function(err, result) {
-          done();
-          if (err)
-           { console.error(err); response.send("Error " + err); }
-          else
-           { response.send(result.rows); }
+app.get('/speakers', (request, response) => {
+    pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+        client.query('SELECT full_name__c, picture_path__c, ( SELECT session_title__c FROM salesforce.Session__c WHERE name = session__c ) FROM salesforce.Speaker__c', (err, result) => {
+            done();
+            if (err){
+                console.error(err);
+                response.send("Error " + err);
+            }else{
+                response.send(result.rows);
+            }
         });
-      });
+    });
+});
+
+app.get('/sessions', (request, response) => {
+    pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+        client.query('SELECT session_title__c, time__c FROM salesforce.Session__c', (err, result) => {
+            done();
+            if(err){
+                console.error(err);
+                reponse.send("Error "+ err);
+            }else{
+                response.send(result.rows);
+            }
+        });
+    });
 });
 
 
