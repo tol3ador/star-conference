043ef813f5e2401ce0b2b33b0a39a0c0d0d0e6fb
Nenad Percic <n.percic@vega.local>
2017-09-29 14:13:23 +0200

    reverting server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require('body-parser');
-var path = require('path');
 var pg = require('pg');
 
 var app = express();
@@ -11,14 +10,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
-  response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  response.send('Hello World!');
 });
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM salesforce.Speaker__c', function(err, result) {
+    client.query('SELECT * FROM Speaker__c', function(err, result) {
       done();
-      console.log(result);
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
