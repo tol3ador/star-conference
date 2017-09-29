15ed23b02e677d399df6f0553081e789fd4b8a3b
Nenad Percic <n.percic@vega.local>
2017-09-29 13:53:56 +0200

    testing postgress connection

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000)
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM Speaker__c', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Listening on " + app.get('port'));
+});
+
--- src/Speakers.js
+++ src/Speakers.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 //import style from 'styled-components';
 
-const DATABASE_URL = 'heroku-postgresql:hobby-dev';
-
-
 class Speakers extends Component {
     render() {
       return (
