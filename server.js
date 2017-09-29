e05e9159498059b2ab57fa2c194db2236fc0a2b1
Nenad Percic <n.percic@vega.local>
2017-09-29 15:09:22 +0200

    reverting

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import pg from 'pg';
 
 var app = express();
@@ -10,17 +9,15 @@ app.set('port', process.env.PORT || 5000)
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/getSpeakers', function(request, response) {
+app.get('/', function(request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT full_name__c, picture_path__c, session__c FROM salesforce.Speaker__c', function(err, result) {
-            done();
-                if (err){
-                    console.error(err); 
-                    response.send("Error " + err); 
-                }else{
-                    response = result.rows;
-                }
-            });
+          done();
+          if (err)
+           { console.error(err); response.send("Error " + err); }
+          else
+           { response.send(result); }
+        });
       });
 });
 
