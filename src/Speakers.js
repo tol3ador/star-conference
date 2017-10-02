cb635b033c82e79edb097482106acd91429d2b96
Nenad Percic <n.percic@vega.local>
2017-10-02 14:47:34 +0200

    testing get request to heroku app

--- src/Speakers.js
+++ src/Speakers.js
@@ -3,30 +3,19 @@ import React, { Component } from 'react';
 import SpeakerItem from './SpeakerItem.js';
 import { List } from 'react-mdl';
 
-const speakersList = [
-    {"full_name__c":"Miloš Davidović","picture_path__c":null,"session_title__c":"Get Real(time) With Websockets"},
-    {"full_name__c":"Nenad Maljugić","picture_path__c":null,"session_title__c":"Umbraco - Tip & Tricks"},
-    {"full_name__c":"Milan Kosanović","picture_path__c":null,"session_title__c":"Visitor Pattern Explained"},
-    {"full_name__c":"Ivana Tešanović","picture_path__c":null,"session_title__c":"Angular 2 Fundamentals & Best practices"},
-    {"full_name__c":"Ognjen Stanić","picture_path__c":null,"session_title__c":"BDD & TDD kroz primere u C#"},
-    {"full_name__c":"Miroslav Stojaković","picture_path__c":null,"session_title__c":"Authentication & Authorization - oAuth2 and Open ID protocols (demonstration of implicit Flow)"},
-    {"full_name__c":"Dejan Ostojić","picture_path__c":null,"session_title__c":"HTTP Caching"},
-    {"full_name__c":"Miroljub Enjaković","picture_path__c":"/servlet/servlet.FileDownload?file=00P0Y000008K7OlUAK","session_title__c":"MVC 5 - Custom model binding"}
-    ];
-
 class Speakers extends Component {
 
     constructor(){
         super();
-        this.state = {speakerList: []}
+        this.state = {speakers: []}
     }
 
     componentDidMount(){
-        fetch(`https://star-conference.herokuapp.com/speakers`)
+        fetch(`/speakers`)
         .then(data => data.json())
         .then(items => {
             this.setState({
-                speakersList: items
+                speakers: items
             })
         });
     }
@@ -35,7 +24,7 @@ class Speakers extends Component {
       return (
         <List>
         {
-            this.state.speakerList.map((speaker, i) => {
+            this.state.speakers.map((speaker, i) => {
                 return <SpeakerItem name={speaker.full_name__c} 
                                     image="https://www.vegaitsourcing.rs/media/1899/nenad-percic_website.jpg"
                                     session={speaker.session_title__c}/>
