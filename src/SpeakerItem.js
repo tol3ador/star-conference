466a1208793dd4a49e862e4cee4f0d8a1a2a705b
Nenad Percic <n.percic@vega.local>
2017-10-04 19:30:52 +0200

    fixed cards

--- src/SpeakerItem.js
+++ src/SpeakerItem.js
@@ -1,33 +1,21 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardActions, Icon } from 'react-mdl';
-import style from 'styled-components';
-
-const One = style.div`
-    width: 256px;
-    height: 256px;
-    background: url('https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAArbAAAAJGRlMmZjYmViLWUyNDItNDUxZC1hZGJmLTgyOGVjZTFmZjQzNA.jpg') center / cover;
-`;
-
-const Two = style.div`
-    height: 52px;
-    padding: 16px;
-    background: rgba(0, 0, 0, 0.2);
-`;
-const Three = style.div`
-    color: #fff;
-    font-size: 14px;
-    font-weight: 500;
-`;
+import { Card, CardTitle, CardActions, Icon, Button } from 'react-mdl';
+//import style from 'styled-components';
 
 class SpeakerItem extends Component {
     render() {
         return (        
-            <One className="demo-card-image mdl-card mdl-shadow--2dp">
-                <div className="mdl-card__title mdl-card--expand"></div>
-                <Three className="mdl-card__actions">
-                    <Two className="demo-card-image__filename">Image.jpg</Two>
-                </Three>
-            </One>
+            <Card shadow={0} style={{width: '256px', height: '256px', background: 'url(https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAArbAAAAJGRlMmZjYmViLWUyNDItNDUxZC1hZGJmLTgyOGVjZTFmZjQzNA.jpg) center / cover', margin: 'auto'}}>
+            <CardTitle expand />
+            <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
+                <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
+                    {this.props.name}
+                </span>
+                <Button>
+                    <Icon name="speaker_notes" style={{color: '#fff'}}></Icon>
+                </Button>
+            </CardActions>
+        </Card>
         );
       }
 }
