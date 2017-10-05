74bf0e0e586d1415ab40a8d4dcb14fabec2e5579
Nenad Percic <n.percic@vega.local>
2017-10-05 12:25:49 +0200

    testing image link

--- src/SpeakerItem.js
+++ src/SpeakerItem.js
@@ -4,17 +4,18 @@ import { Card, CardTitle, CardActions, Icon, Button } from 'react-mdl';
 class SpeakerItem extends Component {
     render() {
         return (        
-            <Card shadow={0} style={{width: '256px', height: '256px', background: 'url(https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAArbAAAAJGRlMmZjYmViLWUyNDItNDUxZC1hZGJmLTgyOGVjZTFmZjQzNA.jpg) center / cover', margin: 'auto'}}>
+            /*<Card shadow={0} style={{width: '256px', height: '256px', background: 'url(https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAArbAAAAJGRlMmZjYmViLWUyNDItNDUxZC1hZGJmLTgyOGVjZTFmZjQzNA.jpg) center / cover', margin: 'auto'}}>
                 <CardTitle expand />
                 <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
                     <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
-                        {this.props.name}
+                        {this.props.image}
                     </span>
                     <Button>
                         <Icon name="speaker_notes" style={{color: '#fff'}}></Icon>
                     </Button>
                 </CardActions>
-            </Card>
+            </Card>*/
+            <span>{this.props.image}</span>
         );
       }
 }
