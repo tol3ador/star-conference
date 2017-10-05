9175578c695f893d221467cd68ddd7c0f0f0c77d
Nenad Percic <n.percic@vega.local>
2017-10-05 14:27:09 +0200

    testing layout shift... no idea

--- src/SpeakerItem.js
+++ src/SpeakerItem.js
@@ -6,17 +6,18 @@ class SpeakerItem extends Component {
     render() {
 
         return (        
-            <Card shadow={0} style={{width: '256px', height: '256px', background: `url(${this.props.image}) center / cover`, margin: 'auto'}}>
-                <CardTitle expand />
-                <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
-                    <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
-                        {this.props.name}
-                    </span>
-                    <Button>
-                        <Icon name="speaker_notes" style={{color: '#fff'}}></Icon>
-                    </Button>
-                </CardActions>
-            </Card>
+            // <Card shadow={0} style={{width: '256px', height: '256px', background: `url(${this.props.image}) center / cover`, margin: 'auto'}}>
+            //     <CardTitle expand />
+            //     <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
+            //         <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
+            //             {this.props.name}
+            //         </span>
+            //         <Button>
+            //             <Icon name="speaker_notes" style={{color: '#fff'}}></Icon>
+            //         </Button>
+            //     </CardActions>
+            // </Card>
+            `WHAT IS THIS`
         );
       }
 }
