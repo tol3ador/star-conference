2f4f48619e7c1b014e1e9a24779c8a2e0e287b56
Nenad Percic <n.percic@vega.local>
2017-10-05 15:44:37 +0200

    grid view for speakers

--- src/SpeakerItem.js
+++ src/SpeakerItem.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { Card, CardTitle, CardActions, Icon, Button } from 'react-mdl';
+import { Card, CardTitle, CardActions, Icon } from 'react-mdl';
 
 class SpeakerItem extends Component {
 
     render() {
 
         return (        
-            <Card shadow={0} style={{width: '300px', height: '300px', background: `url(${this.props.image}) center / cover`, margin: 'auto'}}>
+            <Card shadow={0} style={{width: '150px', height: '150px', background: `url(${this.props.image}) center / cover`, margin: 'auto'}}>
                 <CardTitle expand />
-                <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
-                    <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
+                <CardActions style={{height: '40px', padding: '5px', background: 'rgba(0,0,0,0.15)'}}>
+                    <span style={{color: '#fff', fontSize: '10px', fontWeight: '500'}}>
                         {this.props.name}
                     </span>
-                    <Button>
-                        <Icon name="speaker_notes" style={{color: '#fff'}}></Icon>
-                    </Button>
+                    <Icon name="speaker_notes" style={{color: '#fff'}} />
                 </CardActions>
             </Card>
         );
