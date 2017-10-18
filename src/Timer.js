96845a18b4c0ff380c2156db87632008c5dce7bb
Nenad Percic <n.percic@vega.local>
2017-10-18 14:14:48 +0200

    slight changes and minute display

--- src/Timer.js
+++ src/Timer.js
@@ -39,10 +39,18 @@ class Timer extends Component {
         const alpha = Math.min(1,1-Math.floor(100*this.state.timeLeft/this.state.totalTime)/100);
         const color = alpha !== 1 ? `rgba(64, 116, 146, ${alpha})` : `rgb(241,90,43)`;
 
+        let minutes = Math.abs(this.state.timeLeft < 0 ? Math.ceil(this.state.timeLeft/60000) : Math.floor(this.state.timeLeft/60000));
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+
+        let seconds = Math.abs(Math.floor(this.state.timeLeft/1000)%60);
+        seconds = seconds < 10 ? '0'+seconds : seconds;
+
         return (
             <div className="fullscreen" onClick={this.startCountdown} style={{backgroundColor: color}}>
                 <div className="timer">
-                <h2 className={alpha > 0.50 ? 'white' : 'orange'} >{Math.floor(this.state.timeLeft/1000)}</h2>
+                <h1 className={alpha > 0.50 ? 'white' : 'orange'} style={{fontSize: `${500*(1+alpha)}%`}}>
+                    {`${minutes} : ${seconds}`}
+                </h1>
                 <h3 className='white' style={{display: `${alpha === 1 ? 'block' : 'none'}`}}>Maja Nedučić te ljutito gleda!</h3>
                 </div>
             </div>
