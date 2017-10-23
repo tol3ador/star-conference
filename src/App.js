db5d99478774e9a82b57098b0f20e987c62f0671
Nenad Percic <n.percic@vega.local>
2017-10-23 11:35:19 +0200

    changed partial logo with full logo and edited developer cards sizes and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './resources/logo.svg';
+import logo from './resources/logo.png';
 import Speakers from './components/Speakers/Speakers.js';
 import Timeline from './components/Timeline/Timeline.js';
 import { Tabs, Tab } from 'react-mdl';
@@ -16,8 +16,6 @@ class App extends Component {
       <div>
         <div className="header">
           <img src={logo} alt="logo" className="logo"/>
-          <h1 className="title">Star</h1>
-          <p className="subtitle">Conference</p>
         </div>
         <Tabs activeTab={this.state.activeTab} onChange={(tabId)=> this.setState({activeTab: tabId})} ripple>
             <Tab>Star Track</Tab>
