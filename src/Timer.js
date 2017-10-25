a97509340492baecf18cd6ca57b8d0119840cb03
Nenad Percic <n.percic@vega.local>
2017-10-25 13:26:34 +0200

    removed right click and changed icon test

--- src/Timer.js
+++ src/Timer.js
@@ -101,11 +101,11 @@ class Timer extends Component {
         }
 
         return (
-            <div className={`fullscreen ${backgroundColor} uppercase`} onClick={this.toggleCounter} onContextMenu={this.toggleText}>
+            <div className={`fullscreen ${backgroundColor} uppercase`} onClick={this.toggleCounter} >
                 <div className="timer">
                     <div className={`${controlsColor} bottom-right timer-additional-text`}>
                         {this.state.showText ? null : !firstReminder ? <i class="material-icons">remove_red_eye</i> : null}
-                        {this.state.paused ? <i class="material-icons">play_arrow</i> : <i class="material-icons">pause</i>}
+                        {this.state.paused ? <i class="material-icons">play_circle_outline</i> : <i class="material-icons">pause</i>}
                     </div>
                     <h3 className={`${mainTextColor} timer-main-text`}>{mainText}</h3>
                     <h1 className={`${mainTextColor} timer-numbers`}>
