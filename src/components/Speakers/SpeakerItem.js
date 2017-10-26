196eb4aea2ecc7d73a597e952ce157c2e3489315
Nenad Percic <n.percic@vega.local>
2017-10-26 14:23:57 +0200

    hopefully everything works now

--- src/components/Speakers/SpeakerItem.js
+++ src/components/Speakers/SpeakerItem.js
@@ -4,18 +4,18 @@ import { Card, CardTitle, CardActions, Icon } from 'react-mdl';
 class SpeakerItem extends Component {
     render() {
         return (        
-            <Card shadow={0} className="card speaker" style={{background: `url(${this.props.image}) center / cover`}}>
+            <Card shadow={0} className="card speaker link" style={{background: `url(${this.props.image}) center / cover`}}
+                  onClick={() => this.props.openDialog({
+                    Title: this.props.name,
+                    Id: this.props.speakerId
+                  })
+                }>
                 <CardTitle expand />
                 <CardActions className="card-actions speaker">
                     <span className="card-description speaker font-white">
                         {this.props.name}
                     </span>
-                    <Icon name="speaker_notes" className="link font-white" 
-                            onClick={() => this.props.openDialog({
-                                    Title: this.props.name,
-                                    Id: this.props.speakerId
-                                })
-                            }/>
+                    <Icon name="speaker_notes" className="font-white" />
                 </CardActions>
             </Card>
         );
