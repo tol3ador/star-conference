2263b87278a8b527888f7710a518068446138094
Nenad Percic <n.percic@vega.local>
2017-10-26 15:26:06 +0200

    disabling debug mode for safari

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       activeTab: 0,
-      openDialog: true,
+      openDialog: false,
     };
 
     this.handleOpenDialog = this.handleOpenDialog.bind(this);
